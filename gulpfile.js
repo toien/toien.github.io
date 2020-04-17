f206a85aab4d96da5b3b5a653afbfbace25ca6b7
toien <lshuhuan@gmail.com>
2020-04-17 20:02:43 +0800

    add md5 in dist files' name

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,17 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var cleanCSS = require('gulp-clean-css');
 var del = require('del');
+var hash = require('gulp-hash-filename');
 
 gulp.task('css', function() {
 	var result = gulp.src(['public/css/poole.css', 'public/css/hyde.css', 'public/css/syntax.css'])
 		.pipe(cleanCSS({
 			compatibility: 'ie8'
 		}))
-		.pipe(concat('style.min.css'))
+		.pipe(concat('style.css'))
+		.pipe(hash({
+			"format": "{name}.{hash:5}.min{ext}"
+		}))
 		.pipe(gulp.dest('public/dist'));
 
 	del(['public/dist/*', '!public/dist/*.min.css']);
