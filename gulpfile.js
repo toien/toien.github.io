076ead890d5a8c2db0566327295ae4a77905eec3
toien <sherry@SSSSherry.lan>
2020-06-21 00:10:44 +0800

    add post.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,12 @@ var concat = require('gulp-concat');
 var cleanCSS = require('gulp-clean-css');
 var del = require('del');
 var hash = require('gulp-hash-filename');
+var uglify = require('gulp-uglify');
+
 
 gulp.task('css', function() {
+	del('public/dist/*.min.css');
+
 	var result = gulp.src(['public/css/poole.css', 'public/css/hyde.css', 'public/css/syntax.css'])
 		.pipe(cleanCSS({
 			compatibility: 'ie8'
@@ -15,9 +19,18 @@ gulp.task('css', function() {
 		}))
 		.pipe(gulp.dest('public/dist'));
 
-	del(['public/dist/*', '!public/dist/*.min.css']);
 	return result;
 });
 
 gulp.task('js', function() {
+	del('public/dist/*.min.js');
+
+	var result = gulp.src(['public/js/post.js'])
+		.pipe(uglify())
+		.pipe(hash({
+			"format": "{name}.{hash:5}.min{ext}"
+		}))
+		.pipe(gulp.dest('public/dist'));
+
+	return result;
 });
--- public/js/post.js
+++ public/js/post.js
@@ -1,5 +1,24 @@
 scrollnav.init(document.querySelector('.content .post'), {
-    sections: 'h2',
-    subSections: 'h3',
-    easingStyle: 'easeInOutCubic',
+  sections: 'h2',
+  subSections: 'h3',
+  easingStyle: 'easeInOutCubic',
 });
+
+/**
+* RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+* LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
+*/
+(function () {  // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
+  var commentArea = document.querySelector('#disqus_thread');
+  if (!commentArea) {
+    return;
+  }
+
+  var d = document, s = d.createElement('script');
+  s.src = 'https://toien-github-pages.disqus.com/embed.js';  // IMPORTANT: Replace EXAMPLE with your forum shortname!
+  s.onerror = function () {
+    commentArea.textContent = 'Disqus load failed, try internet scientifically.'
+  };
+  s.setAttribute('data-timestamp', +new Date());
+  (d.head || d.body).appendChild(s);
+})();
